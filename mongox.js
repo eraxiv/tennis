429b77984769d1ac54e23073ff8f41ff8fcb8076
Xavier Greig <eraxiv@gmail.com>
2015-12-07 20:53:36 +1100

    Update mongox.js

--- mongox.js
+++ mongox.js
@@ -1,5 +1,5 @@
 var mongoq =        require("mongoq");
-var mongo =         'mongodb://x:x@192.168.0.130:27017/x';
+var mongo =         'mongodb://[username]:[password]@[connection]:[port]/[db]';
 var db =            mongoq(mongo, { safe: false, strict: false });
 
 module.exports = db;
